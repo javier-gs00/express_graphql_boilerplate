a11e90a73f3be7bc83d702e951d06ae259d0e18a
Javier Gutierrez <javiergs1106@gmail.com>
2018-08-28 23:32:59 -0300

    Add: Handle 404 requests

--- app/index.js
+++ app/index.js
@@ -21,4 +21,9 @@ app.use(function(err, req, res, next) {
 	res.json(err)
 })
 
+// 404 hanlder
+app.use(function(req, res, next) {
+	res.status(404).send("Sorry can't find that!")
+})
+
 module.exports = app
