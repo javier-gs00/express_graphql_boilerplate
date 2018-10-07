077a1606779431120f9a409186d762bf7fe21f8b
Javier Gutierrez <javiergs1106@gmail.com>
2018-10-07 17:47:46 -0300

    Add Dog model

---
+++ app/models/dogs/index.js
@@ -0,0 +1,18 @@
+function Dog(name, breed, age, action) {
+	this.name = name || 'Rin Tin Tin'
+	this.breed = breed || 'German Sheperd'
+	this.age = age || 8
+	this.actions = ['sit', 'bark', 'salute', 'stand', 'jump', 'roll', 'crawl']
+}
+
+Dog.prototype.salute = function() {
+	return `Woof (${this.name} says hi)`
+}
+
+Dog.prototype.performAction = function(action) {
+	if (!this.actions.includes(action)) return `${this.name} can't perform ${action}`
+
+	return `${this.name} will now ${action}`
+}
+
+module.exports = Dog
